0b8fde162020ed1d5537366ce0a53d5489a9583a
peddintibhuvaneswar <134069737+peddintibhuvaneswar@users.noreply.github.com>
2023-07-09 18:21:13 +0530

    script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const createWeatherCard=(cityName,weatherItem,index)=>{
     if(index===0){//HTML for the main weather  card
   return `<div class="details">
            <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-           <h4>Temperature:${(weatherItem.main.temp - 273.15).toFixed(2)} C</h4>
-        <h4>wind:${weatherItem.wind.speed} M/S</h4>
+           <h4>Temperature:${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h4>
+        <h4>Wind:${weatherItem.wind.speed} m/s</h4>
         <h4>Humidity:${weatherItem.main.humidity} %</h4>
           </div>
           <div class="icon">
@@ -22,8 +22,8 @@ const createWeatherCard=(cityName,weatherItem,index)=>{
         return `<div class="card col-lg-2 col-md-4 col-sm-6">
         <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
         <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-        <h4>Temp:${(weatherItem.main.temp - 273.15).toFixed(2)} C</h4>
-        <h4>wind:${weatherItem.wind.speed} M/S</h4>
+        <h4>Temp:${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h4>
+        <h4>Wind:${weatherItem.wind.speed} m/s</h4>
         <h4>Humidity:${weatherItem.main.humidity} %</h4>
     </div>`;
     }
