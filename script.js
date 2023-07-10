2b6d7a18e5d79b17da5d86eb4537ffe2a47e6498
peddintibhuvaneswar <134069737+peddintibhuvaneswar@users.noreply.github.com>
2023-07-10 12:01:24 +0530

    script.js

--- script.js
+++ script.js
@@ -21,7 +21,10 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     } else { // HTML for the other five day forecast card
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+                    <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
